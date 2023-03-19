21772dc2a2b3356ddef9e531b26ace22cf9537e7
JEONGMUN LEE <74344761+leejeongmunn@users.noreply.github.com>
2023-03-19 16:56:56 +0900

    <Fix> status in map

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 var errRequestFailed = errors.New("Request Failed")
 
 func main() {
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.google.com/",
 		"https://www.naver.com/",
@@ -17,16 +18,24 @@ func main() {
 		"https://www.facebook.com/",
 		"https://www.instagram.com/",
 	}
+	results["hello"] = "Hello"
 	for _, url := range urls {
-		fmt.Println("Checking URL... ", url)
-		hitURL(url)
-
+		result := "OK"
+		fmt.Println("Checking :", url)
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
 func hitURL(url string) error {
 	resp, err := http.Get(url)
-	if err == nil || resp.StatusCode >= 400 {
+	if err != nil || resp.StatusCode >= 400 {
 		return errRequestFailed
 	}
 	return nil
